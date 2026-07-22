e1d8ffeadf7966cefac8e9e9a0793b6b0d090af6
Hisham <hisham@globalsurf.ae>
2026-07-22 18:59:10 +0530

    feat: api integration completed for teams page

--- app/components/client/leadership/Index.tsx
+++ app/components/client/leadership/Index.tsx
@@ -1,6 +1,5 @@
 import InnerBanner from "../common/InnerBanner";
 import InnerCta from "../common/InnerCta";
-import { bannerData, ctaData, teamHeaderData } from "./data";
 import ChairmanMessage from "./sections/ChairmanMessage";
 import Team from "./sections/Team";
 import TeamHeader from "./sections/TeamHeader";
